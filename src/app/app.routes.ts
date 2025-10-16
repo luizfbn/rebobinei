369487212a92b5089d7ec6ca8d3fa477cbb46be9
Luiz <luizinhon@hotmail.com>
2025-10-15 23:59:10 -0300

    feat: profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 import { SearchPageComponent } from './features/search/pages/search-page/search-page.component';
+import { ProfilePageComponent } from './features/profile/pages/profile-page/profile-page.component';
 
-export const routes: Routes = [{ path: 'search', component: SearchPageComponent }];
+export const routes: Routes = [
+    {
+        path: 'search',
+        component: SearchPageComponent,
+    },
+    {
+        path: 'profile',
+        component: ProfilePageComponent,
+    },
+];
