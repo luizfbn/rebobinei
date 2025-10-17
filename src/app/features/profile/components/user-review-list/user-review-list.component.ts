0854a11b90440d637a5d47088bd81e1aa5400475
Luiz <luizinhon@hotmail.com>
2025-10-17 00:12:14 -0300

    feat: user-review-list component

---
+++ src/app/features/profile/components/user-review-list/user-review-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, effect, inject, input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserReviewComponent } from '../user-review/user-review.component';
+import { PageSelectorComponent } from '../../../../shared/components/page-selector/page-selector.component';
+
+@Component({
+    selector: 'app-user-review-list',
+    imports: [UserReviewComponent, PageSelectorComponent],
+    templateUrl: './user-review-list.component.html',
+    styleUrl: './user-review-list.component.css',
+})
+export class UserReviewListComponent {
+    router = inject(Router);
+    route = inject(ActivatedRoute);
+
+    userId = input.required<string>();
+    page = input.required<number>();
+
+    reviews = {
+        page: 1,
+        totalPages: 100,
+        totalResults: 2,
+        data: [
+            {
+                id: '123',
+                rating: 4.5,
+                comment: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.`,
+                createdAt: '2025-10-15',
+                movie: {
+                    tmdbId: 123,
+                    title: 'Superman',
+                    originalTitle: 'Superman',
+                    overview: 'Superman bla bla bla',
+                    posterUrl:
+                        'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/xeZ8oG6W60fEPf9yCjERUXiHRBF.jpg',
+                    backdropUrl:
+                        'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/xeZ8oG6W60fEPf9yCjERUXiHRBF.jpg',
+                },
+            },
+        ],
+    };
+
+    constructor() {
+        effect(() => {
+            console.log(`A URL mudou, buscando dados para a página: ${this.page()}`);
+        });
+    }
+
+    navigateToPage(newPage: number) {
+        this.router.navigate([], {
+            relativeTo: this.route,
+            queryParams: { page: newPage },
+            queryParamsHandling: 'merge',
+        });
+    }
+}
--- src/app/features/profile/pages/profile-page/profile-page.component.ts
+++ src/app/features/profile/pages/profile-page/profile-page.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { UserReviewListComponent } from '../../components/user-review-list/user-review-list.component';
 
 @Component({
     selector: 'app-profile-page',
-    imports: [],
+    imports: [UserReviewListComponent],
     templateUrl: './profile-page.component.html',
     styleUrl: './profile-page.component.css',
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent {
+    userId = input.required<string>({
+        alias: 'id',
+    });
+    page = input.required({
+        transform: (page: number | undefined) => Number(page) ?? 1,
+    });
+}
