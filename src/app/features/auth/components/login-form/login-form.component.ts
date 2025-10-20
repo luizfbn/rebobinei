2fe0e4ee42d87b92ddfc23ce147496769b4196b4
Luiz <luizinhon@hotmail.com>
2025-10-20 14:43:15 -0300

    fix(auth/login-form): remove password minimum length validation

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -12,7 +12,7 @@ export class LoginFormComponent {
 
     loginForm = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        password: new FormControl('', [Validators.required]),
     });
 
     get email() {
