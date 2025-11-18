7c09464cea1f6f4da708bd81da5d8aa8456f77e1
Luiz <luizinhon@hotmail.com>
2025-11-17 23:59:27 -0300

    feat(home): popular-movies component

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { PopularMoviesComponent } from '../../components/popular-movies/popular-movies.component';
 
 @Component({
-  selector: 'app-home-page',
-  imports: [],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+    selector: 'app-home-page',
+    imports: [PopularMoviesComponent],
+    templateUrl: './home-page.component.html',
+    styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
