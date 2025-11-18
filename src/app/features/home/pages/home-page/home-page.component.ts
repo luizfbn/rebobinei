a19fa8fdd9bfa3159182a3df9382f20bdda6baa0
Luiz <luizinhon@hotmail.com>
2025-11-18 00:54:57 -0300

    feat(home): upcoming-movies component

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PopularMoviesComponent } from '../../components/popular-movies/popular-movies.component';
+import { UpcomingMoviesComponent } from '../../components/upcoming-movies/upcoming-movies.component';
 
 @Component({
     selector: 'app-home-page',
-    imports: [PopularMoviesComponent],
+    imports: [PopularMoviesComponent, UpcomingMoviesComponent],
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
 })
