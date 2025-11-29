89884ee479034c34da5c0ae5c8ef007c539f7a23
Luiz <luizinhon@hotmail.com>
2025-11-28 22:15:06 -0300

    feat(movies): movie-state service

---
+++ src/app/features/movies/services/movie-state.service.ts
@@ -0,0 +1,57 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { forkJoin, switchMap, tap } from 'rxjs';
+import { ReviewStats } from '../../../core/review/models/review-stats.model';
+import { ReviewWithAuthor } from '../../../core/review/models/review-with-author.model';
+import { MovieDetails } from '../../../core/movie/models/movie-details.model';
+import { Pagination } from '../../../shared/models/pagination.model';
+import { MovieService } from '../../../core/movie/services/movie.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class MovieStateService {
+    movieService = inject(MovieService);
+
+    movie = signal<MovieDetails | null>(null);
+    stats = signal<ReviewStats | null>(null);
+    reviews = signal<Pagination<ReviewWithAuthor>>({
+        data: [],
+        page: 1,
+        totalPages: 1,
+        totalResults: 0,
+    });
+
+    reset() {
+        this.movie.set(null);
+        this.stats.set(null);
+        this.reviews.set({
+            data: [],
+            page: 1,
+            totalPages: 1,
+            totalResults: 0,
+        });
+    }
+
+    loadMovie(movieId: number) {
+        return this.movieService.getDetails(movieId).pipe(tap((movie) => this.movie.set(movie)));
+    }
+
+    loadStats(movieId: number) {
+        return this.movieService.getStats(movieId).pipe(tap((stats) => this.stats.set(stats)));
+    }
+
+    loadReviews(movieId: number) {
+        return this.movieService
+            .getReviews(movieId)
+            .pipe(tap((reviews) => this.reviews.set(reviews)));
+    }
+
+    addReview(...args: Parameters<MovieService['addReview']>) {
+        const movieId = args[0];
+        return this.movieService.addReview(...args).pipe(
+            switchMap(() => {
+                return forkJoin([this.loadReviews(movieId), this.loadStats(movieId)]);
+            })
+        );
+    }
+}
