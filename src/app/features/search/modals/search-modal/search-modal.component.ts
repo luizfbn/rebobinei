d629e56ad298accdc1fde59bdd7312dcdfaf8a94
Luiz <luizinhon@hotmail.com>
2025-12-12 02:09:12 -0300

    feat(search-modal): add modal data on search input

--- src/app/features/search/modals/search-modal/search-modal.component.ts
+++ src/app/features/search/modals/search-modal/search-modal.component.ts
@@ -2,7 +2,9 @@ import { Component, ElementRef, ViewChild, inject, output } from '@angular/core'
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ModalRef } from '../../../../shared/modal/modal-ref';
+import { MODAL_DATA } from '../../../../shared/modal/modal.tokens';
 import { noWhitespaceValidator } from '../../../../shared/validators/no-whitespace.validator';
+import { SearchModalConfig } from './search-modal.types';
 
 @Component({
     selector: 'app-search-modal',
@@ -14,13 +16,17 @@ import { noWhitespaceValidator } from '../../../../shared/validators/no-whitespa
     },
 })
 export class SearchModalComponent {
-    modalRef = inject(ModalRef<string>);
+    modalRef = inject(ModalRef<void>);
+    modalData = inject<SearchModalConfig>(MODAL_DATA);
     router = inject(Router);
 
     onClose = output<void>();
 
     searchForm = new FormGroup({
-        searchInput: new FormControl('', [Validators.required, noWhitespaceValidator]),
+        searchInput: new FormControl(this.modalData?.search ?? '', [
+            Validators.required,
+            noWhitespaceValidator,
+        ]),
     });
 
     @ViewChild('searchInput')
