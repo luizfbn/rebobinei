ffd36b1f6822b46ccd07b2fde81664f4467e4836
Luiz <luizinhon@hotmail.com>
2025-12-15 23:53:09 -0300

    feat: consume data services in home feature

--- src/app/features/home/components/latest-reviews/latest-reviews.component.ts
+++ src/app/features/home/components/latest-reviews/latest-reviews.component.ts
@@ -1,6 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RatingComponent } from '../../../reviews/components/rating/rating.component';
+import { ReviewService } from '../../../../core/review/services/review.service';
+import { Pagination } from '../../../../shared/models/pagination.model';
+import { ReviewDetails } from '../../../../core/review/models/review-details.model';
 
 @Component({
     selector: 'app-latest-reviews',
@@ -9,80 +12,31 @@ import { RatingComponent } from '../../../reviews/components/rating/rating.compo
     styleUrl: './latest-reviews.component.css',
 })
 export class LatestReviewsComponent {
-    reviews = signal({
+    reviewService = inject(ReviewService);
+
+    reviews = signal<Pagination<ReviewDetails>>({
+        data: [],
         page: 1,
-        totalPages: 53730,
-        totalResults: 1074598,
-        data: [
-            {
-                id: '1',
-                rating: 4,
-                comment:
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta nibh quis ullamcorper imperdiet. Donec blandit pellentesque bibendum. Cras tempus nec ipsum vel accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                createdAt: '2025-07-09T00:00:00.000Z',
-                updatedAt: '2025-07-09T00:00:00.000Z',
-                author: {
-                    id: '123',
-                    name: 'Sandro Jorge',
-                    username: 'lilsandro',
-                },
-                movie: {
-                    tmdbId: 123,
-                    title: 'Superman',
-                    originalTitle: 'Superman',
-                    overview:
-                        'Superman, um jovem repórter de Metrópolis, embarca em uma jornada para reconciliar sua herança kryptoniana com sua criação humana como Clark Kent.',
-                    posterUrl:
-                        'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/xeZ8oG6W60fEPf9yCjERUXiHRBF.jpg',
-                    backdropUrl:
-                        'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/xeZ8oG6W60fEPf9yCjERUXiHRBF.jpg',
-                },
-            },
-            {
-                id: '2',
-                rating: 3,
-                comment:
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta nibh quis ullamcorper imperdiet.',
-                createdAt: '2025-07-09T00:00:00.000Z',
-                updatedAt: '2025-07-09T00:00:00.000Z',
-                author: {
-                    id: '122',
-                    name: 'Sandro sandro',
-                    username: 'srsandro',
-                },
-                movie: {
-                    tmdbId: 1062722,
-                    title: 'Frankenstein',
-                    originalTitle: 'Frankenstein',
-                    overview:
-                        'Dr. Victor Frankenstein, um cientista brilhante, mas egoísta, dá vida a uma criatura em um experimento monstruoso que acaba levando à ruína tanto do criador quanto de sua trágica criação.',
-                    posterUrl: 'https://image.tmdb.org/t/p/w500/cXsMxClCcAF1oMwoXZvbKwWoNeS.jpg',
-                    backdropUrl:
-                        'https://image.tmdb.org/t/p/original/hpXBJxLD2SEf8l2CspmSeiHrBKX.jpg',
-                },
-            },
-            {
-                id: '3',
-                rating: 5,
-                comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                createdAt: '2025-07-09T00:00:00.000Z',
-                updatedAt: '2025-07-09T00:00:00.000Z',
-                author: {
-                    id: '121',
-                    name: 'Jorge jorge',
-                    username: 'srjorge',
-                },
-                movie: {
-                    tmdbId: 1054867,
-                    title: 'Uma Batalha Após a Outra',
-                    originalTitle: 'One Battle After Another',
-                    overview:
-                        'Bob Ferguson (DiCaprio), um ex-revolucionário, viveu a juventude como integrante de um grupo de guerrilha, mas agora a sua fracassada vida o atinge em cheio quando o mais cruel de seus inimigos retorna após passar 16 anos e resolve sequestrar sua filha. Diante de tamanha urgência, ele reúne seus antigos companheiros e embarca em um implacável desafio em que precisará correr contra o tempo para salvar quem ele mais ama.',
-                    posterUrl: 'https://image.tmdb.org/t/p/w500/2peYXW6CoruehDnKJGMjl2NuaNB.jpg',
-                    backdropUrl:
-                        'https://image.tmdb.org/t/p/original/zpEWFNqoN8Qg1SzMMHmaGyOBTdW.jpg',
-                },
-            },
-        ],
+        totalPages: 1,
+        totalResults: 0,
     });
+
+    skeletonCount = Array.from({ length: 3 });
+    loading = signal(false);
+
+    constructor() {
+        effect(() => {
+            this.loading.set(true);
+            this.reviewService.list({ limit: 3 }).subscribe({
+                next: (response) => {
+                    this.reviews.set(response);
+                },
+                error: (err) => {
+                    this.loading.set(false);
+                    console.error(err);
+                },
+                complete: () => this.loading.set(false),
+            });
+        });
+    }
 }
--- src/app/features/home/components/upcoming-movies/upcoming-movies.component.ts
+++ src/app/features/home/components/upcoming-movies/upcoming-movies.component.ts
@@ -1,6 +1,9 @@
 import { DatePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MovieService } from '../../../../core/movie/services/movie.service';
+import { Pagination } from '../../../../shared/models/pagination.model';
+import { MovieListItem } from '../../../../core/movie/models/movie-list-item.model';
 
 @Component({
     selector: 'app-upcoming-movies',
@@ -9,41 +12,35 @@ import { RouterLink } from '@angular/router';
     styleUrl: './upcoming-movies.component.css',
 })
 export class UpcomingMoviesComponent {
-    movies = signal({
+    movieService = inject(MovieService);
+
+    movies = signal<Pagination<MovieListItem>>({
+        data: [],
         page: 1,
-        totalPages: 53730,
-        totalResults: 1074598,
-        data: [
-            {
-                tmdbId: 1062722,
-                title: 'Frankenstein',
-                originalTitle: 'Frankenstein',
-                overview:
-                    'Dr. Victor Frankenstein, um cientista brilhante, mas egoísta, dá vida a uma criatura em um experimento monstruoso que acaba levando à ruína tanto do criador quanto de sua trágica criação.',
-                releaseDate: '2025-10-17T00:00:00.000Z',
-                posterUrl: 'https://image.tmdb.org/t/p/w500/cXsMxClCcAF1oMwoXZvbKwWoNeS.jpg',
-                backdropUrl: 'https://image.tmdb.org/t/p/original/hpXBJxLD2SEf8l2CspmSeiHrBKX.jpg',
-            },
-            {
-                tmdbId: 1248226,
-                title: 'Um Dia Fora de Controle',
-                originalTitle: 'Playdate',
-                overview:
-                    'Quando o contador desempregado Brian leva seu filho para brincar com o filho de Jeff, pai que fica em casa, ele espera um dia tranquilo. Em vez disso, eles acabam perseguidos por mercenários, e Brian — totalmente despreparado — precisa sobreviver a um obstáculo absurdo atrás do outro. A vida de pai de família vira um caos de filme de ação nesta aventura hilária.',
-                releaseDate: '2025-11-05T00:00:00.000Z',
-                posterUrl: 'https://image.tmdb.org/t/p/w500/auLOTlulof19GSPrNomyG7myfzN.jpg',
-                backdropUrl: 'https://image.tmdb.org/t/p/original/5lQ4euO30sDin5nCifvi0vURFNd.jpg',
-            },
-            {
-                tmdbId: 1054867,
-                title: 'Uma Batalha Após a Outra',
-                originalTitle: 'One Battle After Another',
-                overview:
-                    'Bob Ferguson (DiCaprio), um ex-revolucionário, viveu a juventude como integrante de um grupo de guerrilha, mas agora a sua fracassada vida o atinge em cheio quando o mais cruel de seus inimigos retorna após passar 16 anos e resolve sequestrar sua filha. Diante de tamanha urgência, ele reúne seus antigos companheiros e embarca em um implacável desafio em que precisará correr contra o tempo para salvar quem ele mais ama.',
-                releaseDate: '2025-09-23T00:00:00.000Z',
-                posterUrl: 'https://image.tmdb.org/t/p/w500/2peYXW6CoruehDnKJGMjl2NuaNB.jpg',
-                backdropUrl: 'https://image.tmdb.org/t/p/original/zpEWFNqoN8Qg1SzMMHmaGyOBTdW.jpg',
-            },
-        ],
+        totalPages: 1,
+        totalResults: 0,
     });
+
+    skeletonCount = Array.from({ length: 3 });
+    loading = signal(false);
+
+    constructor() {
+        effect(() => {
+            this.loading.set(true);
+            this.movieService.list({ category: 'upcoming' }).subscribe({
+                next: (response) => {
+                    const limitedResponse = {
+                        ...response,
+                        data: response.data.slice(0, 3),
+                    };
+                    this.movies.set(limitedResponse);
+                },
+                error: (err) => {
+                    this.loading.set(false);
+                    console.error(err);
+                },
+                complete: () => this.loading.set(false),
+            });
+        });
+    }
 }
