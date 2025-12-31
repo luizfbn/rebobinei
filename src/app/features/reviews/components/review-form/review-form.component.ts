df371cddf903f5534eda7826dab55d63e3e5c76b
Luiz <luizinhon@hotmail.com>
2025-12-30 22:20:44 -0300

    style(reviews): refine review form component

--- src/app/features/reviews/components/review-form/review-form.component.ts
+++ src/app/features/reviews/components/review-form/review-form.component.ts
@@ -1,12 +1,13 @@
 import { Component, effect, input, output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
 import { noWhitespaceValidator } from '../../../../shared/validators/no-whitespace.validator';
 import { RatingComponent } from '../rating/rating.component';
 import { ReviewForm } from '../../models/review-form.model';
 
 @Component({
     selector: 'app-review-form',
-    imports: [ReactiveFormsModule, RatingComponent],
+    imports: [ReactiveFormsModule, RatingComponent, DecimalPipe],
     templateUrl: './review-form.component.html',
     styleUrl: './review-form.component.css',
 })
@@ -14,9 +15,14 @@ export class ReviewFormComponent {
     reviewSubmit = output<ReviewForm>();
     isLoading = input<boolean>(false);
 
+    commentMaxLength = 2000;
+
     reviewForm = new FormGroup({
         rating: new FormControl(0, [Validators.min(1), Validators.max(5)]),
-        comment: new FormControl('', [Validators.maxLength(2000), noWhitespaceValidator]),
+        comment: new FormControl('', [
+            Validators.maxLength(this.commentMaxLength),
+            noWhitespaceValidator,
+        ]),
     });
 
     constructor() {
